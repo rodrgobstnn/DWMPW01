fd5e6835015fb9cc8056f0db9b0662f3f4aa9692
rodrgobstnn <rodrgo.flores2003@gmail.com>
2026-08-17 17:48:22 -0400

    DWMPW01 incorporacion de navbar

--- index.php
+++ index.php
@@ -1,12 +1,30 @@
-<html>
+<!DOCTYPE html>
+<html lang="es">
     <head>
         <title>Mi primera pagina web</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
     </head>
     <body>
-        <a href="empresa.php">Ir a empresa</a><br>
-        <a href="productos.php">Ir a productos</a><br>
-        <a href="servicios.php">Ir a servicios</a><br>
-        <a href="contacto.php">Ir a contacto</a><br>
+        <!barra de navegacion>
+        <nav class ="navbar navbar navbar-expand-sm bg-dark navbar-dark">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#"><img src="gym.png" alt="logo" height="36"></a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+                </button>
+            </div>
+        </nav>
+
+
+
+        <div class="container-fluid bg-secondary">
+            <a href="empresa.php">Ir a empresa</a><br>
+            <a href="productos.php">Ir a productos</a><br>
+            <a href="servicios.php">Ir a servicios</a><br>
+            <a href="contacto.php">Ir a contacto</a><br>
+        </div>
     </body>
 </html>
 
--- productos.php
+++ productos.php
@@ -1,6 +1,11 @@
-<html>
+<!DOCTYPE html>
+<html lang="es">
     <head>
         <title>Mi primer producto</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
     </head>
     <body>
         Hola productos <br>
